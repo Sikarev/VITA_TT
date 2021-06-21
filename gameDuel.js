d668e643c588fce0a19e96c3a1a233f1a7637085
Roma <sikarev.roman@yandex.ru>
2021-06-21 18:06:32 +0300

    added fool protection and no-repeat cards setting

--- gameDuel.js
+++ gameDuel.js
@@ -11,8 +11,8 @@ let botPick, penaltyPoints;
 
 let setCards = (firstPl, secondPl, cardsPerPlayer) => {
     for (let i = 0; i < cardsPerPlayer; i++) {
-        firstPl.push(Math.floor(Math.random()*12))
-        secondPl.push(Math.floor(Math.random()*12))
+        firstPl.push(i);
+        secondPl.push(i);
     }
 }
 let showCards = (hand) => {
@@ -29,6 +29,12 @@ let pickACard = (pick, hand) => {
             return;
         }
     }
+}
+let pickIsCorrect = (pick, hand) => {
+    for (let i = 0; i < hand.length; i++) {
+        if (pick == hand[i]) return true;
+    }
+    return false;
 }
 let pickACardAI = (hand, playersTurn) => {
     if (!playersTurn) {
@@ -55,6 +61,11 @@ let pickACardAI = (hand, playersTurn) => {
         return min;
     }
 }
+let setPenalty = (attackPts, defendPts) => {
+    let penaltyPoints = attackPts - defendPts;
+    if (penaltyPoints < 0) penaltyPoints = 0;
+    return penaltyPoints;
+}
 
 const { listenerCount } = require("events");
 const readline = require("readline");
@@ -65,47 +76,104 @@ setCards(p1.cards, p2.cards, cardsPerPlayer);
 console.log("Ваши карты: ");
 showCards(p1.cards);
 r1.on('line', (input) => {
+    if (pickIsCorrect(input.trim(), p1.cards)) {
     
-    //Игрок выбирает карту
-    pickACard(input.trim(), p1.cards);
-    showCards(p1.cards);
+        //Игрок выбирает карту
+        pickACard(input.trim(), p1.cards);
+        showCards(p1.cards);
 
-    //Бот выбирает карту
-    // showCards(p2.cards);
-    botPick = pickACardAI(p2.cards, yourTurn);
-    // showCards(p2.cards);
+        //Бот выбирает карту
+        botPick = pickACardAI(p2.cards, yourTurn);
 
-    if (yourTurn) {
-        console.log("Ваша атака: " + input.trim());
-        console.log("Защита противника: " + botPick);
-        penaltyPoints = parseInt(input.trim()) - botPick;
-        if (penaltyPoints < 0) penaltyPoints = 0;
-        p2.penaltyPts += penaltyPoints;
+        if (yourTurn) {
+            console.log("Ваша атака: " + input.trim());
+            console.log("Защита противника: " + botPick);
+            penaltyPoints = setPenalty(parseInt(input.trim()), botPick);
+            p2.penaltyPts += penaltyPoints;
+        } else {
+            console.log("Ваша защита: " + input.trim());
+            console.log("Атака противника: " + botPick);
+            penaltyPoints = setPenalty(botPick, parseInt(input.trim()));
+            p1.penaltyPts += penaltyPoints;
+        }
+        console.log("Штрафных очков начислено: " + penaltyPoints);
+
+        yourTurn = !yourTurn;
+
+        if (p1.cards.length == 0) {
+            console.log("У игроков кончились карты");
+            console.log("Количество штрафных очков: ");
+            console.log("Вы: " + p1.penaltyPts);
+            console.log("ИИ: " + p2.penaltyPts);
+
+            if (p1.penaltyPts < p2.penaltyPts) console.log("Вы победили!");
+            else if (p1.penaltyPts > p2.penaltyPts) console.log("Вы проиграли");
+            else console.log("Ничья");
+
+            r1.close();
+        }
     } else {
-        console.log("Ваша защита: " + input.trim());
-        console.log("Атака противника: " + botPick);
-        penaltyPoints = botPick - parseInt(input.trim());
-        if (penaltyPoints < 0) penaltyPoints = 0;
-        p1.penaltyPts += penaltyPoints;
-    }
-    console.log("Штрафных очков начилено: " + penaltyPoints);
-
-    yourTurn = !yourTurn;
-
-    if (p2.cards.length == 0) {
-        console.log("У игроков кончились карты");
-        console.log("Количество штрафных очков: ");
-        console.log("Вы: " + p1.penaltyPts);
-        console.log("ИИ: " + p2.penaltyPts);
-
-        if (p1.penaltyPts < p2.penaltyPts) console.log("Вы победили!");
-        else if (p1.penaltyPts > p2.penaltyPts) console.log("Вы проиграли");
-        else console.log("Ничья");
-
-        r1.close();
+        console.log("Такой карты у вас нет. Попробуйте ещё раз");
+        showCards(p1.cards);
     }
 
 }).on('close', function() {
     console.log("Конец.");;
     process.exit(0);
 })
+
+// let pickRandomCard = (hand) => {
+//     let pickIndex = Math.floor(Math.random()*hand.length);
+//     let result = hand[pickIndex];
+//     hand.splice(pickIndex, 1);
+//     return result;
+// }
+// let rndPick;
+
+// console.log("Ваши карты: ");
+// showCards(p1.cards);
+// r1.on('line', (input) => {
+    
+//         //Игрок выбирает карту
+//         rndPick = pickRandomCard(p1.cards);
+//         showCards(p1.cards);
+
+//         //Бот выбирает карту
+//         // showCards(p2.cards);
+//         botPick = pickACardAI(p2.cards, yourTurn);
+//         // showCards(p2.cards);
+
+//         if (yourTurn) {
+//             console.log("Ваша атака: " + rndPick);
+//             console.log("Защита противника: " + botPick);
+//             penaltyPoints = rndPick - botPick;
+//             if (penaltyPoints < 0) penaltyPoints = 0;
+//             p2.penaltyPts += penaltyPoints;
+//         } else {
+//             console.log("Ваша защита: " + rndPick);
+//             console.log("Атака противника: " + botPick);
+//             penaltyPoints = botPick - rndPick;
+//             if (penaltyPoints < 0) penaltyPoints = 0;
+//             p1.penaltyPts += penaltyPoints;
+//         }
+//         console.log("Штрафных очков начилено: " + penaltyPoints);
+
+//         yourTurn = !yourTurn;
+
+//         if (p1.cards.length == 0) {
+//             console.log("У игроков кончились карты");
+//             console.log("Количество штрафных очков: ");
+//             console.log("Вы: " + p1.penaltyPts);
+//             console.log("ИИ: " + p2.penaltyPts);
+
+//             if (p1.penaltyPts < p2.penaltyPts) console.log("Вы победили!");
+//             else if (p1.penaltyPts > p2.penaltyPts) console.log("Вы проиграли");
+//             else console.log("Ничья");
+
+//             r1.close();
+//         }
+
+// }).on('close', function() {
+//     console.log("Конец.");;
+//     process.exit(0);
+// })
